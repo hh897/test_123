29835283c2fe6ab16e51e5d246ff10f2181ea19e
hh897 <57852735+hh897@users.noreply.github.com>
2021-12-08 15:38:16 +0700

    Add files via upload

---
+++ js/script-web.js
@@ -0,0 +1,50 @@
+DATA_BUKU = [
+	{
+		image: "assets/img/kategori/cantikitu.png",
+		judul: "Cantik Itu Luka",
+		ket: "Buku cantik itu luka.",
+		harga: 18000
+	},
+	{
+		image: "assets/img/kategori/senjahujan.png",
+		judul: "Senja Hujan",
+		ket: "buku senja hujan",
+		harga: 23000
+	},
+	{
+		image: "assets/img/kategori/kisahtanahjawa.png",
+		judul: "Kisah Tanah Jawa",
+		ket: "buku kisah tanah jawa",
+		harga: 24000
+	},
+	{
+		image: "assets/img/kategori/antares.png",
+		judul: "Antares",
+		ket: "buku antares",
+		harga: 17000
+	},
+	{
+		image: "assets/img/kategori/senjahujan.png",
+		judul: "Senja Hujan",
+		ket: "buku senja hujan",
+		harga: 23000
+	},
+	{
+		image: "assets/img/kategori/kisahtanahjawa.png",
+		judul: "Kisah Tanah Jawa",
+		ket: "buku kisah tanah jawa",
+		harga: 24000
+	},
+	{
+		image: "assets/img/kategori/antares.png",
+		judul: "Antares",
+		ket: "buku antares",
+		harga: 17000
+	}
+]
+var app = new Vue({
+	el: "#app",
+	data: {
+		dataBuku: DATA_BUKU
+	}
+})
